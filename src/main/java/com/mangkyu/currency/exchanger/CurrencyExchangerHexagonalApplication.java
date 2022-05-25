1db13566f502bb006129ce5565ce8662ff81e7b2
MangKyu <whalsrb1226@gmail.com>
2022-05-25 14:48:32 +0900

    초기 커밋

---
+++ src/main/java/com/mangkyu/currency/exchanger/CurrencyExchangerHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.mangkyu.currency.exchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangerHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangerHexagonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mangkyu/currency/exchanger/CurrencyExchangerHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangkyu.currency.exchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangerHexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
