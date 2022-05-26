ebffe4e30f64cfd8b9818dcb67a3c662394598ad
MangKyu <whalsrb1226@gmail.com>
2022-05-27 01:13:07 +0900

    ExchangeException 리팩토링

--- src/main/java/com/mangkyu/currency/exchanger/app/exchange/error/ExchangeException.java
+++ src/main/java/com/mangkyu/currency/exchanger/app/exchange/error/ExchangeException.java
@@ -1,13 +1,12 @@
 package com.mangkyu.currency.exchanger.app.exchange.error;
 
 import com.mangkyu.currency.exchanger.app.common.errors.CommonException;
-import com.mangkyu.currency.exchanger.app.common.errors.ErrorCode;
 import lombok.Getter;
 
 @Getter
 public class ExchangeException extends CommonException {
 
-    public ExchangeException(final ErrorCode errorCode) {
+    public ExchangeException(final ExchangeErrorCode errorCode) {
         super(errorCode);
     }
 
