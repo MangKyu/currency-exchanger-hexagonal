8806edf22743d6e8087b346b94c84a7fda885ebc
MangKyu <whalsrb1226@gmail.com>
2022-06-01 15:37:55 +0900

    정적 자원 처리를 위한 WebMvc 설정 추가

---
+++ src/main/java/com/mangkyu/currency/exchanger/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.mangkyu.currency.exchanger.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {"classpath:/static/", "classpath:/", "classpath:/resources/"};
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+}
