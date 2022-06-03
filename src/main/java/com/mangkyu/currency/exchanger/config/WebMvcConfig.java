5013e591f9e5dd0adb0b6134c0d0cda7f1821cfa
MangKyu <whalsrb1226@gmail.com>
2022-06-03 23:52:38 +0900

    WebMvcConfig 리팩토링

--- src/main/java/com/mangkyu/currency/exchanger/config/WebMvcConfig.java
+++ src/main/java/com/mangkyu/currency/exchanger/config/WebMvcConfig.java
@@ -7,12 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {"classpath:/static"};
-
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+                .addResourceLocations("classpath:/static");
     }
 
 }
