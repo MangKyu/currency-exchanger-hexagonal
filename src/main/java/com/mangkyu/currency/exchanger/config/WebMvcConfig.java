67dfa32e3e5b6b69a5bd8829fa39a7fbede43375
MangKyu <whalsrb1226@gmail.com>
2022-06-06 21:16:22 +0900

    웹 포워딩 설정

--- src/main/java/com/mangkyu/currency/exchanger/config/WebMvcConfig.java
+++ src/main/java/com/mangkyu/currency/exchanger/config/WebMvcConfig.java
@@ -1,7 +1,9 @@
 package com.mangkyu.currency.exchanger.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,6 +13,12 @@ class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static");
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/exchange");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 
 }
