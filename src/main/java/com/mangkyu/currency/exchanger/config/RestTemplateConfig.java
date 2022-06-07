403df0b7f38e57cec2f4adb18c9d7189cbf2ff10
MangKyu <whalsrb1226@gmail.com>
2022-06-07 23:26:31 +0900

    RestTemplate 설정 고도화(타임아웃, 재시도, 로깅)

--- src/main/java/com/mangkyu/currency/exchanger/config/RestTemplateConfig.java
+++ src/main/java/com/mangkyu/currency/exchanger/config/RestTemplateConfig.java
@@ -1,15 +1,75 @@
 package com.mangkyu.currency.exchanger.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.*;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.stream.Collectors;
 
 @Configuration
 class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .additionalInterceptors(clientHttpRequestInterceptor(), new LoggingInterceptor())
+                .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+                .build();
+    }
+
+    public ClientHttpRequestInterceptor clientHttpRequestInterceptor() {
+        return (request, body, execution) -> {
+            RetryTemplate retryTemplate = new RetryTemplate();
+            retryTemplate.setRetryPolicy(new SimpleRetryPolicy(3));
+            try {
+                return retryTemplate.execute(context -> execution.execute(request, body));
+            } catch (Throwable throwable) {
+                throw new RuntimeException(throwable);
+            }
+        };
+    }
+
+    @Slf4j
+    static class LoggingInterceptor implements ClientHttpRequestInterceptor {
+
+        @Override
+        public ClientHttpResponse intercept(HttpRequest req, byte[] body, ClientHttpRequestExecution ex) throws IOException {
+            final String sessionNumber = makeSessionNumber();
+            printRequest(sessionNumber, req, body);
+            ClientHttpResponse response = ex.execute(req, body);
+            printResponse(sessionNumber, response);
+            return response;
+        }
+
+        private String makeSessionNumber() {
+            return Integer.toString((int) (Math.random() * 1000000));
+        }
+
+        private void printRequest(final String sessionNumber, final HttpRequest req, final byte[] body) {
+            log.info("[{}] URI: {}, Method: {}, Headers:{}, Body:{} ",
+                    sessionNumber, req.getURI(), req.getMethod(), req.getHeaders(), new String(body, StandardCharsets.UTF_8));
+        }
+
+        private void printResponse(final String sessionNumber, final ClientHttpResponse res) throws IOException {
+            String body = new BufferedReader(new InputStreamReader(res.getBody(), StandardCharsets.UTF_8)).lines()
+                    .collect(Collectors.joining("\n"));
+
+            log.info("[{}] Status: {}, Headers:{}, Body:{} ",
+                    sessionNumber, res.getStatusCode(), res.getHeaders(), body);
+        }
     }
 
 }
