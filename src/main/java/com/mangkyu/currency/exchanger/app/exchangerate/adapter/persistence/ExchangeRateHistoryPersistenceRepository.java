faddd7ed6e5c4c6bbe6a674f3a353c94c6f29f91
MangKyu <whalsrb1226@gmail.com>
2022-06-14 22:49:48 +0900

    ExchangeRateHistory의 Source와 Target, CreatedBy 역순으로 조회하는 쿼리 추가

--- src/main/java/com/mangkyu/currency/exchanger/app/exchangerate/adapter/persistence/ExchangeRateHistoryPersistenceRepository.java
+++ src/main/java/com/mangkyu/currency/exchanger/app/exchangerate/adapter/persistence/ExchangeRateHistoryPersistenceRepository.java
@@ -1,6 +1,9 @@
 package com.mangkyu.currency.exchanger.app.exchangerate.adapter.persistence;
 
+import com.mangkyu.currency.exchanger.app.money.domain.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExchangeRateHistoryPersistenceRepository extends JpaRepository<ExchangeRateHistoryEntity, Long> {
+
+    ExchangeRateHistoryEntity findTopBySourceIsAndTargetIsOrderByCreatedAtDesc(final Currency source, final Currency target);
 }
--- src/test/java/com/mangkyu/currency/exchanger/app/exchangerate/adapter/persistence/ExchangeRateHistoryPersistenceRepositoryTest.java
+++ src/test/java/com/mangkyu/currency/exchanger/app/exchangerate/adapter/persistence/ExchangeRateHistoryPersistenceRepositoryTest.java
@@ -1,12 +1,23 @@
 package com.mangkyu.currency.exchanger.app.exchangerate.adapter.persistence;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static com.mangkyu.currency.exchanger.app.exchangerate.testbase.ExchangeTestBase.sourceCurrency;
+import static com.mangkyu.currency.exchanger.app.exchangerate.testbase.ExchangeTestBase.targetCurrency;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 class ExchangeRateHistoryPersistenceRepositoryTest {
 
     @Autowired
     private ExchangeRateHistoryPersistenceRepository target;
+
+    @Test
+    void temp() {
+        final ExchangeRateHistoryEntity result = target.findTopBySourceIsAndTargetIsOrderByCreatedAtDesc(sourceCurrency, targetCurrency);
+        assertThat(result).isNull();
+    }
 
 }
